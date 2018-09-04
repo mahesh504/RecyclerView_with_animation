d5d2173e989902dd77cea5d3afeca39366b5b173
Mahesh M <mallemmahesh@gmail.com>
2018-09-04 18:38:51 +0530

    Added new Animation

--- app/src/main/java/com/mahesh504/recycleview_sample/util/AnimationUtil.java
+++ app/src/main/java/com/mahesh504/recycleview_sample/util/AnimationUtil.java
@@ -13,6 +13,9 @@ public class AnimationUtil {
         @SuppressLint("ObjectAnimatorBinding") ObjectAnimator animatorTransilateY= ObjectAnimator.ofFloat(holder.itemView,"translationY",goesdown==true ? 200 :-200,0);
         animatorTransilateY.setDuration(1200);
 
+//                @SuppressLint("ObjectAnimatorBinding") ObjectAnimator animatorTransilateX= ObjectAnimator.ofFloat(holder.itemView,"translationX",200,0);
+//        animatorTransilateX.setDuration(1500);
+
         animatorSet.playTogether(animatorTransilateY);
         animatorSet.start();
     }
